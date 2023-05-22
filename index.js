f8b0e89dc9e83533ceadf3c0295524c8f809f593
Assaad Akl <assaad.akl@outlook.com>
2023-05-22 22:11:20 +0300

    2nd commit

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 //git does not ruplicate the data abdnn he just makes it historical with date of updates
-//
+//git init 3ml initialize sar hyda l file b2lbo git la yra2bo
+//git status 3m shuf min mr2b w min laa
+//git add 1.index.js sar hl2 mra2ab hyda l file
+//git commit -m "1st comit" w "2nd comit" teni yom 5lsst sho8ol lal yom w batal yra2b la lyom
+//git log hon y3ni bi2rjini shu 8yrna w min 8yr amtan
+// b5od l code w bktob git checkout past lal code bytl3li shu 3mlna bhyda l nhar
+
+//hyd part l jdid
